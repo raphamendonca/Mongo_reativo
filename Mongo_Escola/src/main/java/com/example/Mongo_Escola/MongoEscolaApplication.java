44f708a7a0c29be0c5709cd1a789e176ee7eb838
Rapha Mendonca <raphamendonca@protonmail.com>
2022-04-29 19:51:08 -0300

    Adicionando projeto base

---
+++ Mongo_Escola/src/main/java/com/example/Mongo_Escola/MongoEscolaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Mongo_Escola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoEscolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoEscolaApplication.class, args);
+	}
+
+}
---
+++ Mongo_Escola/src/test/java/com/example/Mongo_Escola/MongoEscolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Mongo_Escola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoEscolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
